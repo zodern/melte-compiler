cd047c9275e683eef2162bb8f5c7d454d4ad6d29
zodern <zodern@users.noreply.github.com>
2021-04-21 12:13:45 -0500

    Release 1.0.1

---
+++ changelog.md
@@ -0,0 +1,7 @@
+## 1.0.1 - April 21, 2021
+
+- Fix error when package.json does not have `svelte:compiler` options
+
+## 1.0.0 - April 19, 2021
+
+- Initial version
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zodern:melte-compiler',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Compiler used by zodern:melte',
   git: 'https://github.com/zodern/melte-compiler.git',
   documentation: 'README.md'
